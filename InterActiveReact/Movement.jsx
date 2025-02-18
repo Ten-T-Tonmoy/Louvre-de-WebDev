b6f8d11024257653a909b269337225cd885a1884
Farhn Hassan Tonmoy <161760230+Ten-T-Tonmoy@users.noreply.github.com>
2025-02-18 23:39:12 +0600

    oops was wrong file

--- InterActiveReact/Movement.jsx
+++ InterActiveReact/Movement.jsx
@@ -1,79 +1,34 @@
+import React, { useState } from "react";
 import "./index.css";
-export default function Movement() {
+//image slider time
+//cant use push on useState
+//items center= vertically justify-center=vertically centers
+let x = 0;
+let y = 0;
+document.addEventListener("keydown", (event) => {
+  event.preventDefault();
+  const box = document.getElementById("box");
+  if (event.key === "d") {
+    y += 15;
+  } else if (event.key === "a") {
+    y -= 15;
+  } else if (event.key === "w") {
+    x -= 15;
+  } else if (event.key === "s") {
+    x += 15;
+  }
+  box.style.margin = `${x}px ${y}px`;
+});
+//rendering will make x and y 0 thts why out of export func
+export default function App() {
   return (
-    <>
-      <div className="bg-black">
-        <div className="flex-wrap flex justify-center items-center p-10 gap-4">
-          <button
-            className="bg-yellow-500 text-white px-4 py-2 rounded
-           transform scale-100 hover:scale-125
-            transition duration-300 ease-in-out
-             p-4  "
-          >
-            Hover me
-          </button>
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded
-           transform scale-100 hover:scale-110
-            transition duration-300 ease-in-out
-             p-4  "
-          >
-            Hover me
-          </button>
-          <button
-            className="bg-pink-500 text-white px-4 py-2 rounded
-           transform scale-100  hover:scale-110
-            transition duration-300 ease-in-out
-             p-4  "
-          >
-            Hover me
-          </button>
-          <button
-            className="bg-violet-500 text-white px-4 py-2 rounded
-           transform scale-100 hover:scale-110
-            transition duration-300 ease-in-out
-             p-4  "
-          >
-            Hover me
-          </button>
-        </div>
-        <p
-          className="text-center text-[100px]
-       text-green-400 font-bold "
-        >
-          Hi Nig!
-        </p>
-        <div className="flex flex-wrap justify-center items-center p-10">
-          <div
-            className="bg-red-600 h-60 p-10 rounded-lg m-[20px]
-               font-mono border-8 border-red-400
-               font-bold overflow-hidden transition-all hover:scale-105"
-          >
-            This is box 1
-          </div>
-          <div
-            className="bg-cyan-600 h-60 p-10 rounded-lg m-[20px]
-               font-mono border-8 border-cyan-400
-               font-bold overflow-hidden transition-all hover:scale-105"
-          >
-            This is box 2
-          </div>
-          <div
-            className="bg-orange-600 h-60 p-10 rounded-lg m-[20px]
-               font-mono border-8 border-orange-400
-               font-bold overflow-hidden transition-all hover:scale-105"
-          >
-            This is box 3
-          </div>
-          <div
-            className="bg-purple-600 h-60 p-10 rounded-lg m-[20px]
-               font-mono border-8 border-purple-400
-               font-bold overflow-hidden transition-all hover:scale-105"
-          >
-            This is box 4
-          </div>
-        </div>
+    <div id="box" className="h-40 bg-green-400 w-40 ">
+      <div
+        className="text-white font-mono 
+      text-[100px] font-bold text-center"
+      >
+        😐
       </div>
-    </>
+    </div>
   );
 }
